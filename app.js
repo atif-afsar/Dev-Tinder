e1992563979dbd1d5c9a02bf62f89077fd373318
Atif Afsar <atifafsar70@gmail.com>
2025-06-30 18:58:59 +0530

    Middleware & Error handler

--- app.js
+++ app.js
@@ -4,19 +4,32 @@ const app = express();
 
 
 app.get("/user", (req, res) => {
-  res.send("Hello, User!");
+ next();
 });
 app.post("/user", (req, res) => {
-  res.send("User created successfully!");
+ next();
 });
 
 app.delete("/user", (req, res) => {
-  res.send("User deleted successfully!");
+ next();
 });
 
 app.patch("/user", (req, res) => {
   res.send("User updated successfully!");
 });
+
+app.use("/getUserData", (req, res, next) => {
+  //logic of DB call and get user data 
+})
+throw new Error("This is an error!");
+res.send("User data sent");
+
+app.use("/error", (err, req, res, next) =>{
+  if(err){
+    // log the error
+    res.status(500).send("Something went wrong!");
+  }
+})
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
