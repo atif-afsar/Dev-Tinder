814012d582d190ed1ba9cec9a8188d42701eefa3
Atif Afsar <atifafsar70@gmail.com>
2025-07-01 21:57:28 +0530

    Adding data dynamically

--- middleware/app.js
+++ middleware/app.js
@@ -3,18 +3,22 @@ const { connectDB } = require("../config/database");
 const User = require("../models/user"); // ✅ Declare FIRST
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); //middleware given by express to parse JSON bodies
 
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Atif",
-    lastName: "Afsar",
-    emailId: "atif@gmail.com",
-    password: "atif@123",
-  });
-
+  const user = new User(req.body)
+  //   {
+  //   firstName: "virat",
+  //   lastName: "kohli",
+  //   emailId: "virat@gmail.com",
+  //   password: "virat@123",
+  // });
+   try{
   await user.save();
   res.send("User added successfully");
+   }catch (err) {
+    console.error("Error saving user:", err);
+   }
 });
 
 connectDB()
