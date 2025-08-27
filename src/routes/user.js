efab3858d137c0815fa82dabdee1b921fe96251f
Atif Afsar <atifafsar70@gmail.com>
2025-08-27 15:03:49 +0530

    feat: Add endpoint to retrieve accepted user connections and improve response structure

--- src/routes/user.js
+++ src/routes/user.js
@@ -23,4 +23,32 @@ userRouter.get("/user/request/received", userAuth, async (req, res) => {
     }
 });
 
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try{
+    const loggedInUser = req.user;
+    const connectionRequests = await connectionRequest.find({
+      $or: [
+        { fromUserId: loggedInUser._id, status: "accepted" },
+        { toUserId: loggedInUser._id, status: "accepted" }
+      ]
+    }).populate("fromUserId", USER_SAVE_DATA).populate("toUserId", USER_SAVE_DATA);
+
+
+
+    const data = connectionRequests.map((row) =>{ 
+      if(row.fromUserId._id.toString() === loggedInUser._id.toString()){
+        return row.toUserId;  
+      }
+      return row.fromUserId;
+    });
+
+    res.json({
+      message: "Data received successfully",
+      data: connectionRequests
+    });
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
+  }
+})
+
 module.exports = userRouter;
